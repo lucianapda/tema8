ddb3f26839a214a65d6c248ef7b074330f14d7e4
guiaschafer <Guilherme@LAPTOP-M5KJUU5O>
2017-11-08 19:20:43 -0200

    projeto inicial front end

---
+++ backend/TrabalhoFrontEnd/Controllers/LaboratorioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabalhoFrontEnd.Controllers
+{
+    public class LaboratorioController : Controller
+    {
+        // GET: Laboratorio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ backend/TrabalhoFrontEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrabalhoFrontEnd.MvcApplication" Language="C#" %>
